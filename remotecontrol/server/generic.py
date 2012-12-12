3a071e8db076f0a2b242d5bd24c34bd9abd4cef4
Mike Boers <mboers@keypics.com>
2012-12-12 15:16:15 -0800

    Annnnd back together again...

--- remotecontrol/server/generic.py
+++ remotecontrol/server/generic.py
@@ -1,6 +1,8 @@
-import code
-import sys
 import socket
+import sys
+import threading
+import re
+import code
 
 
 class _fileobject(socket._fileobject):
@@ -74,6 +76,9 @@ class Interpreter(code.InteractiveConsole):
         if len(lines) > 1 and lines[-1].strip():
             return True
         
+        return self._runsource(source, *args)
+
+    def _runsource(self, source, *args):
         return code.InteractiveInterpreter.runsource(self, source, *args)
     
     def interact(self):
@@ -83,3 +88,69 @@ class Interpreter(code.InteractiveConsole):
             self.server.debug('shutting down client: %r', self.addr)
             self.sock.close()
             self.file.close
+
+
+class Server(object):
+    
+    interpreter_class = Interpreter
+
+    def __init__(self, addr, locals=None):
+        self.addr = addr
+        self.locals = locals
+        self.exec_lock = threading.Lock()
+    
+    def debug(self, msg, *args):
+        if args:
+            msg = msg % args
+        print '# rc:', msg
+
+    def listen(self):
+        
+        self.debug('starting server %r', self.addr)
+        
+        # Create the server socket.
+        self.sock = socket.socket(socket.AF_INET)
+        self.sock.bind(self.addr)
+        self.sock.listen(0)
+        
+        try:
+            while True:
+                
+                sock, addr = self.sock.accept()
+                self.debug('new connection: %r', addr)
+                
+                # Spawn a thread with a a client handler.
+                client = self.interpreter_class(self, self.locals, sock, addr)
+                threading.Thread(target=client.interact).start()
+        
+        except KeyboardInterrupt:
+            pass
+        
+        finally:
+            self.debug('closing: %r', addr)
+            self.sock.close()
+
+
+
+
+def listen(addr, locals=None):
+    if locals is None:
+        locals = {}
+    if isinstance(addr, int):
+        addr = ('', addr)
+    console = Server(addr, locals)
+    console.listen()
+
+
+def spawn(*args, **kwargs):
+    thread = threading.Thread(target=listen, args=args, kwargs=kwargs)
+    thread.start()
+    return threan
+
+
+if __name__ == '__main__':
+
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 9000
+    listen(('', port))
+
+
