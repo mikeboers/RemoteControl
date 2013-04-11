69ea203b4c145fc249029e6306062f102f083d0f
Mike Boers <mboers@keypics.com>
2013-04-11 10:05:43 -0700

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    
+    name='remotecontrol',
+    version='1.0.0-dev',
+    description='A remote REPL and command socket.',
+    url='http://github.com/westernx/remotecontrol',
+    
+    packages=['remotecontrol'],
+    
+    author='Mike Boers',
+    author_email='remotecontrol@mikeboers.com',
+    license='BSD-3',
+    
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
